09e67e741e7da8822946304b37570299589855ff
mscarlos107 <mscarlos107@gmail.com>
2024-11-28 19:04:50 +0100

    Versión base de clases GameBoard,GameBoardTest y Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,11 +5,5 @@ import view.GameView;
 import controller.GameController;
 
 public class Main {
-    public static void main(String[] args) {
-        GameBoard gameBoard = new GameBoard();
-        GameView gameView = new GameView();
-        GameController gameController = new GameController(gameBoard);
 
-        gameController.startGame();
-    }
 }
--- src/test/java/model/GameBoardTest.java
+++ src/test/java/model/GameBoardTest.java
@@ -1,4 +1,3 @@
-
 package model;
 
 import org.junit.jupiter.api.Test;
